4f63fd71bbbdc1789ecbda859b42b8a34f061baa
kritika malhotra <kritikamalhotra2016@gmail.com>
2021-07-06 14:20:35 -0700

    first commit

---
+++ myproject/src/secrets.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyD7WpxYt5IYPvadj_eY1eRm-qZ31Z2EGMY",
+    authDomain: "reels-2a267.firebaseapp.com",
+    projectId: "reels-2a267",
+    storageBucket: "reels-2a267.appspot.com",
+    messagingSenderId: "310751759772",
+    appId: "1:310751759772:web:81fac59e4ff04dc9c80c59"
+}
